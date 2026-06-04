e3b9fcf2ec5acbd806a27b6ab12b99b8ae14a139
beijocas <messias.benja14@gmail.com>
2026-06-04 16:37:01 -0300

    Update test_jurossimples.c

--- test_jurossimples.c
+++ test_jurossimples.c
@@ -2,13 +2,11 @@
 #include <assert.h>
 #include "function_jurossimples.c"
 
-int main()
+void main()
 {
-    assert(montante(100, 0.5, 10) == 500);
-    assert(montante(100, 0.25, 12) == 300);
-    assert(montante(100, 0.75, 5) == 375);
+    assert(m(100, 0.5, 10) == 500);
+    assert(m(100, 0.25, 12) == 300);
+    assert(m(100, 0.75, 5) == 375);
 
     printf("teste executado com sucesso!");
-    
-    return 0;
 }
